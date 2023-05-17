7a55b67dd0674435f4645f0db090120524b3d969
fe-luna <feluna.zhang@gmail.com>
2023-05-17 21:45:42 +0800

    todo-hover

--- src/components/todo-hover/index.tsx
+++ src/components/todo-hover/index.tsx
@@ -3,9 +3,9 @@ import "./style.scss";
 const TodoHover = () => {
   return (
     <div className="todo-hover">
-      <Icon name="edit" />
-      <Icon name="expiration" />
-      <Icon name="comment" />
+      <Icon name="edit" className="todo-hover--margin" />
+      <Icon name="expiration" className="todo-hover--margin" />
+      <Icon name="comment" className="todo-hover--margin" />
       <Icon name="more" className="todo-hover__more" />
     </div>
   );
